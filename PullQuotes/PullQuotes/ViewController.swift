e2a52c4e6c325b5d5e12246e06e69ac3c6f62ad1
danielhour <daniel.hour@me.com>
2017-07-08 14:52:31 -0700

    Xcode Project Init

---
+++ PullQuotes/PullQuotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PullQuotes
+//
+//  Created by Daniel Hour on 7/8/17.
+//  Copyright © 2017 Daniel Hour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
